f9757a0beb431bb324288f3e8c1875e5f2530a95
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2025-03-31 15:17:47 -0300

    added base page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,18 @@
 import { Navigate, Route, Routes } from 'react-router';
 import BaseLayout from './layouts/BaseLayout';
 import ListServiceOrdersPage from '@/features/orders/ListServiceOrdersPage';
+import CreateServiceOrderPage from './features/orders/CreateServiceOrderPage';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route element={<BaseLayout />}>
         <Route index element={<Navigate to="orders" />} />
-        <Route path="orders" element={<ListServiceOrdersPage />} />
+        <Route path="orders">
+          <Route index element={<ListServiceOrdersPage />} />
+          <Route path="create" element={<CreateServiceOrderPage />} />
+        </Route>
+
         <Route path="products" element={<p>Produtos</p>} />
         <Route path="expenses" element={<p>Despesas</p>} />
 
